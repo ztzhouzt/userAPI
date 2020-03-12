edc594e4005dcfa675ab804a2987fb9617c973ea
zhou <1249126625@qq.com>
2020-03-12 11:37:34 +0800

    3324324

--- User.Identity/Program.cs
+++ User.Identity/Program.cs
@@ -13,7 +13,7 @@ namespace User.Identity
     public class Program
     {
         /// <summary>
-        /// 启动 
+        /// 启动    
         /// </summary>
         /// <param name="args"></param>
         public static void Main(string[] args)
