be6550e4cd1171d0ee0ca0a45c038128a9502f41
zhou <1249126625@qq.com>
2020-03-12 10:38:38 +0800

    添加项目文件。

---
+++ UserAPI/Models/BPFile.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserAPI.Models
+{
+    public class BFile
+    {
+        /// <summary>
+        /// BP Id
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// 用户Id
+        /// </summary>
+        public int AppUserId { get; set; }
+
+        /// <summary>
+        /// 文件名称
+        /// </summary>
+        public string FileName { get; set; }
+
+        /// <summary>
+        /// 上传的源文件的地址
+        /// </summary>
+        public string OriginFilePath { get; set; }
+
+         /// <summary>
+         /// 格式转换后的文件地址
+         /// </summary>
+        public string FromatFilePath { get; set; }
+
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        public DateTime CreateTime { get; set; }
+
+
+
+    }
+}
---
+++ UserAPI/Models/UserTag.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserAPI.Models
+{
+    public class UserTag
+    {
+        public int AppUserId { get; set; }
+
+        public string Tag { get; set; }
+
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        public DateTime CreateTime { get; set; }
+    }
+}
